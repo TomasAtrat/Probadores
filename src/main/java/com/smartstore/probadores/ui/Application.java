228ffe2dd273b6235505012a997119f78966839a
tomasAtrat <tomasatrat@gmail.com>
2022-11-25 21:55:47 -0300

    publish version

--- src/main/java/com/smartstore/probadores/ui/Application.java
+++ src/main/java/com/smartstore/probadores/ui/Application.java
@@ -7,6 +7,8 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
@@ -20,10 +22,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @PWA(name = "Probadores", shortName = "Probadores", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @Push
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
--- src/main/java/com/smartstore/probadores/ui/backend/utils/constants/WebServicesConstants.java
+++ src/main/java/com/smartstore/probadores/ui/backend/utils/constants/WebServicesConstants.java
@@ -1,5 +1,5 @@
 package com.smartstore.probadores.ui.backend.utils.constants;
 
 public class WebServicesConstants {
-    public static final String ADD_TASK_SERVICE = "http://localhost:8081/api/v1/tasks/task";
+    public static final String ADD_TASK_SERVICE = "http://localhost:8081/SmartStoreWebService/api/v1/tasks/task";
 }
